eb3ab24effaa8991a01b3019043529194f49730a
Juan José Angarita Yela <129020936+RamonaFlores@users.noreply.github.com>
2024-07-13 17:24:35 -0500

    Landing page advances
    created Landing Page, updated the index.css file and made minor changes to other components. Also installed in the npm the styled components package to allow beter personalization

--- front/client/src/pages/Error.jsx
+++ front/client/src/pages/Error.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
-
+import { Link, useRouteError } from 'react-router-dom';
 export const Error = () => {
+  const error = useRouteError()
+  console.log(error)
   return (
     <div>
       <h1> Error Page</h1>
--- front/client/src/pages/Landing.jsx
+++ front/client/src/pages/Landing.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from '../assets/images/main.svg'
 
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
+ 
 export const Landing = () => {
   return (
-    <div>Landing</div>
-  )
-}
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1> Rastrea tu <span> Chamba</span></h1>
+          <p>
+          Aquí puedes llevar el control de todas tus postulaciones de manera sencilla y divertida. Olvídate del caos de los correos y las notas dispersas, y mantente al tanto de tus entrevistas, ofertas y rechazos en un solo lugar.
+          </p>
+          <Link to= '/register' className="btn register-link">
+            ¡Regístrate!
+          </Link>
+          <Link to= '/register' className="btn ">
+              Login / Demostración
+          </Link>
+        </div>
+        <img src={main} alt= 'perrito'className= 'img main-img' />
+      </div>
+    </Wrapper>
+  );
+};
+// una forma tradicional de crear un componente estilizado
+/*
+const StyledBtn = styled.button`
+  font-size:1.5rem;
+  background: red;
+  color: white;
+`;
+*/
+//ejemplo 2
+/*
+const Wrapper = styled.div`
+  background: red;
+  h1 {
+    color: white;
+  }
+  .content {
+    background: blue;
+    color: yellow;
+  }
+`;
+*/
 export default Landing;
