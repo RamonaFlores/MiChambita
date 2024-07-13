0f905150740a8c9dbf0f0bc1034dca52a8cd9128
Juan José Angarita Yela <129020936+RamonaFlores@users.noreply.github.com>
2024-07-13 17:42:20 -0500

    404 Error Page done
    Finished Error Page

--- front/client/src/pages/Error.jsx
+++ front/client/src/pages/Error.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 import { Link, useRouteError } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import Fourimage from '../assets/images/notFound.svg'
 export const Error = () => {
   const error = useRouteError()
   console.log(error)
+  if (error.status===404){
+    return (
+    <Wrapper>
+      <div>
+        <img src={Fourimage} alt = 'not found'/>
+        <h3>Caray! Pagina no encontrada </h3>
+        <p>No pudimos encontrar la página que estabas buscando</p>
+        <Link to='/dashboard'>Vuelta al Inicio</Link>
+      </div>
+    </Wrapper>
+    );
+  }
   return (
-    <div>
-      <h1> Error Page</h1>
-      <Link to = '/'> back home </Link>
-    </div>
+    <Wrapper>
+      <div>
+        <h3> Algo salió mal</h3>
+      </div>
+
+     
+    </Wrapper>
   )
 }
 export default Error;
